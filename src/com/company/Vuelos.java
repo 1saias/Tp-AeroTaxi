3d28f37eccac9e8f7d6aca8417975df98ee59dee
Misael Flores <misaelflores4190@gmail.com>
2020-06-20 22:24:40 -0300

    Se actualizo el repositorio local

--- src/com/company/Vuelos.java
+++ src/com/company/Vuelos.java
@@ -11,7 +11,6 @@ public class Vuelos {
     private  int kilometros;
     private  Usuario usuario;
     private Aviones aeronave;
-<<<<<<< HEAD
     private float importe;
 
 
@@ -29,11 +28,11 @@ public class Vuelos {
         this.acompañantes = acompañantes;
     }
 
-    public int getKilomentros() {
-        return kilomentros;
+    public int getKilometros() {
+        return kilometros;
     }
-    public void setKilomentros(int kilomentros) {
-        this.kilomentros = kilomentros;
+    public void setKilometros(int kilometros) {
+        this.kilometros = kilometros;
     }
 
     public Usuario getUsuario() {
@@ -58,12 +57,14 @@ public class Vuelos {
         this.importe = importe;
     }
 
-    public Vuelos(ArrayList vuelos, LocalDateTime fecha, int acompañantes, int kilomentros, Usuario usuario, Aviones aeronave){
+    public Vuelos(LocalDateTime fecha, int acompañantes,int kilomentros, Usuario usuario, Aviones aeronave, Ciudades Origen, Destinos destinos) {
         this.fecha = fecha;
         this.acompañantes = acompañantes;
-        this.kilomentros = kilomentros;
+        this.kilometros = kilomentros;
         this.usuario = usuario;
         this.aeronave = aeronave;
+        this.Origen = Origen;
+        this.destinos = destinos;
     }
 
     public float CalcularCostoTotal(int kilomentros,Aviones aeronave,int acompañantes){
@@ -83,7 +84,7 @@ public class Vuelos {
         if(!(obj instanceof Vuelos))return false;
         Vuelos vl=(Vuelos)obj;
         return this.fecha.equals(vl.fecha) && this.acompañantes == vl.acompañantes &&
-                this.kilomentros == vl.kilomentros && this.usuario.equals(usuario) &&
+                this.kilometros == vl.kilometros && this.usuario.equals(usuario) &&
                 this.aeronave.equals(aeronave);
     }
 
@@ -94,25 +95,16 @@ public class Vuelos {
         return "Vuelos{" +
                 "fecha=" + fecha +
                 ", acompañantes=" + acompañantes +
-                ", kilomentros=" + kilomentros +
+                ", kilomentros=" + kilometros +
                 ", usuario=" + usuario +
                 ", aeronave=" + aeronave +
                 '}';
     }
-=======
     private Ciudades Origen;
     private Destinos destinos;
 
 
-    public Vuelos(LocalDateTime fecha, int acompañantes,int kilomentros, Usuario usuario, Aviones aeronave, Ciudades Origen, Destinos destinos) {
-        this.fecha = fecha;
-        this.acompañantes = acompañantes;
-        this.kilometros = kilomentros;
-        this.usuario = usuario;
-        this.aeronave = aeronave;
-        this.Origen = Origen;
-        this.destinos = destinos;
-    }
+
 
    /* public int TotalDeKm(Ciudades origen,Destinos destinos) {
         if (origen.equals(destinos) )
@@ -130,56 +122,4 @@ public class Vuelos {
         Cordoba,Santiago,Montevideo
     }
 
-    public LocalDateTime getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDateTime fecha) {
-        this.fecha = fecha;
-    }
-
-    public int getAcompañantes() {
-        return acompañantes;
-    }
-
-    public void setAcompañantes(int acompañantes) {
-        this.acompañantes = acompañantes;
-    }
-
-    public int getKilometros() {
-        return kilometros;
-    }
-
-    public void setKilometros(int kilometros) {
-        this.kilometros = kilometros;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public Aviones getAeronave() {
-        return aeronave;
-    }
-
-    public void setAeronave(Aviones aeronave) {
-        this.aeronave = aeronave;
-    }
-
-    @Override
-    public String toString() {
-        return "Vuelos{" +
-                "fecha=" + fecha +
-                ", acompañantes=" + acompañantes +
-                ", kilometros=" + kilometros +
-                ", usuario=" + usuario +
-                ", aeronave=" + aeronave +
-                ", ciudades=" + Origen +
-                ", destinos=" + destinos +
-                '}';
-    }
 }
